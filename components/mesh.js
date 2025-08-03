f6adefbfd4b61de7688a82ed48b41209da237667
Samuel Fortunato <samfortunato@gmail.com>
2025-08-03 14:34:10 -0700

    Add descriptions for kind of unfamiliar component types

--- components/mesh.js
+++ components/mesh.js
@@ -1,5 +1,6 @@
 import { Component } from '../engine/component.js';
 
+/** An entity's bounds. */
 export class Mesh extends Component {
 	constructor(w, h) {
 		super();
--- components/shape.js
+++ components/shape.js
@@ -1,5 +1,6 @@
 import { Component } from '../engine/component.js';
 
+/** An entity's visual form. */
 export class Shape extends Component {
 	constructor(options) {
 		super();
